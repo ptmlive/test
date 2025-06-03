55529746a245527458dd871c6cd0daf34606f677
ptmlive <piotr.krzysztof.maslanka@live.com>
2025-06-03 10:07:35 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,47 +1,16 @@
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-
-import java.util.List;
-
-@Configuration
-@EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
-@RequiredArgsConstructor
-public class SecurityConfiguration {
-
-    @Value("${scp.http.security.authenticated:}")
-    private final List<String> authenticatedEndpoints;
-
-    @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers(authenticatedEndpoints.toArray(new String[0])).authenticated()
-                        .anyExchange().permitAll()
-                )
-                .build();
-    }
-}
-
-
 @Component
 @Order(-1)
-@RequiredArgsConstructor
 @Slf4j
 public class AuthenticationFilter implements GlobalFilter {
 
     @Value("${scp.http.security.authenticated:}")
     private List<String> authenticatedEndpoints;
 
-    @Lazy
-    private final ScpAuthServerClient scpAuthServerClient;
+    private final ObjectProvider<ScpAuthServerClient> scpAuthServerClientProvider;
+
+    public AuthenticationFilter(ObjectProvider<ScpAuthServerClient> scpAuthServerClientProvider) {
+        this.scpAuthServerClientProvider = scpAuthServerClientProvider;
+    }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -61,11 +30,14 @@ public class AuthenticationFilter implements GlobalFilter {
         String token = authHeaders.get(0).substring(7);
 
         try {
-            CheckedToken checkedToken = scpAuthServerClient.checkToken(new UserToken(token));
+            CheckedToken checkedToken = scpAuthServerClientProvider.getObject()
+                .checkToken(new UserToken(token));
+
             ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
                     .header("X-User-id", checkedToken.getEmployeeId())
                     .build();
             return chain.filter(exchange.mutate().request(mutatedRequest).build());
+
         } catch (Exception ex) {
             log.warn("Token validation failed", ex);
             return unauthorized(exchange, "Invalid token");
