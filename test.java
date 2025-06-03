74f04ee11e17ec4f13eeaae8191153eeaebfe73c
ptmlive <piotr.krzysztof.maslanka@live.com>
2025-06-03 13:59:00 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,76 +1,47 @@
 package com.example.gateway.filters;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.TraceContext;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.Ordered;
-import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.context.annotation.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import reactor.core.publisher.Mono;
-
-import java.net.URI;
 
 @Slf4j
 @Component
-public class BasePathRewriteFilter implements GlobalFilter, Ordered {
+@RequiredArgsConstructor
+@ConditionalOnProperty(name = "spring.sleuth.enabled", havingValue = "true", matchIfMissing = true)
+public class AddRequestIdHeaderFilter implements GlobalFilter, Ordered {
 
-    private final ReactiveDiscoveryClient discoveryClient;
-
-    public BasePathRewriteFilter(ReactiveDiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
-    }
+    private final Tracer tracer;
 
     @Override
     public int getOrder() {
-        return 10160;
+        return Ordered.LOWEST_PRECEDENCE;
     }
 
     @Override
-    public Mono<Void> filter(ServerWebExchange exchange, org.springframework.cloud.gateway.filter.GatewayFilterChain chain) {
-        URI lbUri = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR);
-        if (lbUri == null) {
-            log.debug("No LB URI found; skipping basePath rewrite");
-            return chain.filter(exchange);
-        }
-
-        var route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
-        if (route == null) {
-            log.debug("No route attribute found; skipping basePath rewrite");
-            return chain.filter(exchange);
-        }
-
-        String serviceId = route.getUri().getHost();
-        String instanceHost = lbUri.getHost();
-        int instancePort = lbUri.getPort();
-
-        return discoveryClient.getInstances(serviceId)
-                .filter(inst -> inst.getHost().equals(instanceHost) && inst.getPort() == instancePort)
-                .next()
-                .flatMap(inst -> {
-                    String basePath = inst.getMetadata().get("basepath");
-                    if (!StringUtils.hasText(basePath)) {
-                        log.debug("Instance {}:{} has no basepath metadata; skipping rewrite", instanceHost, instancePort);
-                        return chain.filter(exchange);
-                    }
-
-                    String originalPath = lbUri.getPath();
-                    String newPath = "/" + basePath + originalPath;
-                    URI rewritten = UriComponentsBuilder.fromUri(lbUri)
-                            .replacePath(newPath)
-                            .build(true)
-                            .toUri();
-
-                    log.debug("Rewriting path from '{}' to '{}' for service '{}'", originalPath, newPath, serviceId);
-                    exchange.getAttributes().put(ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR, rewritten);
-                    return chain.filter(exchange);
-                })
-                .switchIfEmpty(Mono.defer(() -> {
-                    log.debug("No matching instance found for {}:{}; skipping basePath rewrite", instanceHost, instancePort);
-                    return chain.filter(exchange);
-                }));
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            Span currentSpan = tracer.currentSpan();
+            if (currentSpan == null) {
+                log.debug("No current span available; skipping x-request-id header");
+                return;
+            }
+            TraceContext context = currentSpan.context();
+            String traceId = context != null ? context.traceIdString() : null;
+            if (traceId == null) {
+                log.debug("Trace ID is null; skipping x-request-id header");
+                return;
+            }
+            log.info("Adding x-request-id header with trace ID {}", traceId);
+            exchange.getResponse().getHeaders().add("x-request-id", traceId);
+        }));
     }
 }
