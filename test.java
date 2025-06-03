ae2b32ed928075f3002651530c701f45e1385021
ptmlive <piotr.krzysztof.maslanka@live.com>
2025-06-03 14:19:26 +0200

    Update test.java

--- test.java
+++ test.java
@@ -1,13 +1,6 @@
-package com.example.gateway.filters;
-
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.sleuth.Span;
-import org.springframework.cloud.sleuth.Tracer;
-import org.springframework.cloud.sleuth.TraceContext;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.cloud.gateway.filter.Ordered;
-import org.springframework.context.annotation.ConditionalOnProperty;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
@@ -15,33 +8,20 @@ import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
-@ConditionalOnProperty(name = "spring.sleuth.enabled", havingValue = "true", matchIfMissing = true)
-public class AddRequestIdHeaderFilter implements GlobalFilter, Ordered {
-
-    private final Tracer tracer;
-
-    @Override
-    public int getOrder() {
-        return Ordered.LOWEST_PRECEDENCE;
-    }
+@Order(102)
+public class AddServiceIdHeaderFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-            Span currentSpan = tracer.currentSpan();
-            if (currentSpan == null) {
-                log.debug("No current span available; skipping x-request-id header");
-                return;
+            Object serviceIdObj = exchange.getAttribute("serviceId");
+            if (serviceIdObj != null) {
+                String serviceId = serviceIdObj.toString();
+                log.info("Adding x-service-id header with value {}", serviceId);
+                exchange.getResponse().getHeaders().add("x-service-id", serviceId);
+            } else {
+                log.debug("No serviceId attribute; skipping x-service-id header");
             }
-            TraceContext context = currentSpan.context();
-            String traceId = context != null ? context.traceIdString() : null;
-            if (traceId == null) {
-                log.debug("Trace ID is null; skipping x-request-id header");
-                return;
-            }
-            log.info("Adding x-request-id header with trace ID {}", traceId);
-            exchange.getResponse().getHeaders().add("x-request-id", traceId);
         }));
     }
 }
